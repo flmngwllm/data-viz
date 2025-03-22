3cb7f2d02d340dde9bf38b9510fda5d1ae57e290
flmngwllm <flmngwllm232@gmail.com>
2025-03-22 14:12:40 -0400

    add CORS support and configure Django settings; include analytics_app in installed apps

--- analytics_project/settings.py
+++ analytics_project/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
+    'rest_framework',
+    'analytics_app',
 ]
 
 MIDDLEWARE = [
@@ -47,7 +50,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ORIGINS = True
 
 ROOT_URLCONF = 'analytics_project.urls'
 
